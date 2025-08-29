9b56a02977e5fd5b05338b871754d95b0d293284
Satish Banchhere <satishbanchhere1@gmail.com>
2025-08-30 00:30:53 +0530

    remvoed unnecessary stuff

--- app/api/geotagging/candidates/route.ts
+++ app/api/geotagging/candidates/route.ts
@@ -1,6 +1,6 @@
 import {NextRequest, NextResponse} from "next/server";
 
-export async function GET(req: NextRequest, res: NextResponse) {
+export async function GET(req: NextRequest) {
     try {
         const searchParams = req.nextUrl.searchParams;
         const lat = searchParams.get("lat");
